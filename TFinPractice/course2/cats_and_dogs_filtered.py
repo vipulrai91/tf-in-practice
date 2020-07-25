b133bc426d09301c5ebbf91521e431b85e34b07c
Vipul Rai <vipulrai8891@gmail.com>
2020-07-25 21:35:54 +0530

    catas and dogs basics

---
+++ TFinPractice/course2/cats_and_dogs_filtered.py
@@ -0,0 +1,107 @@
+import os
+import tensorflow as tf
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.losses import binary_crossentropy
+
+base_dir = "/Users/vipul/PycharmProjects/TFExams/TFinPractice/data/cats_and_dogs_filtered/cats_and_dogs_filtered"
+
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+
+# Directory with our training cat/dog pictures
+train_cats_dir = os.path.join(train_dir, 'cats')
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+
+# Directory with our validation cat/dog pictures
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+
+train_cat_fnames = os.listdir(train_cats_dir)
+train_dog_fnames = os.listdir(train_dogs_dir)
+
+print(train_cat_fnames[:10])
+print(train_dog_fnames[:10])
+
+print('total training cat images :', len(os.listdir(train_cats_dir)))
+print('total training dog images :', len(os.listdir(train_dogs_dir)))
+
+print('total validation cat images :', len(os.listdir(validation_cats_dir)))
+print('total validation dog images :', len(os.listdir(validation_dogs_dir)))
+
+# total training cat images : 1000
+# total training dog images : 1000
+# total validation cat images : 500
+# total validation dog images : 500
+
+
+# import matplotlib.image as mpimg
+# import matplotlib.pyplot as plt
+#
+# # Parameters for our graph; we'll output images in a 4x4 configuration
+# nrows = 4
+# ncols = 4
+#
+# pic_index = 0  # Index for iterating over images
+#
+# # Set up matplotlib fig, and size it to fit 4x4 pics
+# fig = plt.gcf()
+# fig.set_size_inches(ncols * 4, nrows * 4)
+#
+# pic_index += 8
+#
+# next_cat_pix = [os.path.join(train_cats_dir, fname)
+#                 for fname in train_cat_fnames[pic_index - 8:pic_index]
+#                 ]
+#
+# next_dog_pix = [os.path.join(train_dogs_dir, fname)
+#                 for fname in train_dog_fnames[pic_index - 8:pic_index]
+#                 ]
+#
+# for i, img_path in enumerate(next_cat_pix + next_dog_pix):
+#     # Set up subplot; subplot indices start at 1
+#     sp = plt.subplot(nrows, ncols, i + 1)
+#     sp.axis('Off')  # Don't show axes (or gridlines)
+#
+#     img = mpimg.imread(img_path)
+#     plt.imshow(img)
+#
+# plt.show()
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(128, (3, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+# print(model.summary())
+
+model.compile(optimizer=RMSprop(lr=0.001), loss=binary_crossentropy, metrics=['accuracy'])
+
+# Data pre-processing
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale=1.0 / 255.)
+valid_datagen = ImageDataGenerator(rescale=1.0 / 255.)
+
+train_generator = train_datagen.flow_from_directory(train_dir,
+                                                    batch_size=20,
+                                                    class_mode='binary',
+                                                    target_size=(150, 150))
+
+validation_generator = valid_datagen.flow_from_directory(validation_dir,
+                                                         batch_size=20,
+                                                         class_mode='binary',
+                                                         target_size=(150, 150))
+
+history = model.fit(train_generator,
+                    validation_data=validation_generator,
+                    steps_per_epoch=100,
+                    epochs=15,
+                    validation_steps=50,
+                    verbose=2)
--- TFinPractice/readme.md
+++ TFinPractice/readme.md
@@ -4,3 +4,7 @@ For fixing cert errors while downloading data sets
 open /Applications/Python\ 3.7/Install\ Certificates.command
 
 ```
+
+```buildoutcfg
+!pip install tensorflow==2.0.0-alpha0 
+```
